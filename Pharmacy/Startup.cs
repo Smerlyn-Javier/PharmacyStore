9e06c19e6cace9dc5d15563f7dea0be1c2f65bd0
Marluan <marluanespiritusantoguerrero@gmail.com>
2019-09-29 19:04:06 -0400

    [ADD]: dependecy injections configured

--- Pharmacy/Startup.cs
+++ Pharmacy/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Pharmacy.Models;
+using Pharmacy.Repositories;
+using Pharmacy.Services;
 
 namespace Pharmacy
 {
@@ -22,16 +17,16 @@ namespace Pharmacy
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        private IConfiguration Configuration { get; }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<DrugRepository>();
+            services.AddTransient<DrugService>();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,7 +34,7 @@ namespace Pharmacy
                 app.UseDeveloperExceptionPage();
             }
 
-//            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
